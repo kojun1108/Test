a8e784d0c42023b15d249d733cb694212d9eb35f
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-06 08:35:58 +0900

    CartItemDto.java を作成

---
+++ CartItemDto.java
@@ -0,0 +1,13 @@
+public class CartItemDto {
+
+    private String itemType; // HOTEL or FLIGHT
+
+    private Integer itemId;
+
+    private String itemName;
+
+    private Integer price;
+
+    private Integer quantity;
+
+}
