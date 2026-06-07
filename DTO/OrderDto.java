c71eefbc60bb59087d3e929d097dabce4fc863b8
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 10:57:38 +0900

    Update OrderDto.java

--- DTO/OrderDto.java
+++ DTO/OrderDto.java
@@ -1,1 +1,11 @@
-public 
+public class OrderDto{
+
+    private List<CartItemDto> items = new ArrayList<>();
+
+    public Integer getTotalPrice() {
+
+        return items.stream()
+                .mapToInt(x -> x.getPrice() * x.getQuantity())
+                .sum();
+    }
+}
