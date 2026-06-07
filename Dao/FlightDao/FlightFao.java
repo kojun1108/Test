ba21c75f5de7d08fb19fa93bbc0583666d17cf6d
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 13:38:39 +0900

    Update FlightFao.java

--- Dao/FlightDao/FlightFao.java
+++ Dao/FlightDao/FlightFao.java
@@ -1,1 +1,13 @@
+@Dao
+@ConfigAutowireable
+public interface FlightDao {
 
+    @Select
+    List<Flight> selectAll();
+
+    @Select
+    Flight selectByItemcode(String itemCode);
+
+    @Update(sqlFile = true)
+    int update(Flight flight);
+}
