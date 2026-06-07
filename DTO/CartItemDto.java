28b27a6c949b1da572bee520536ea99a6960cfb2
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 14:20:05 +0900

    Update CartItemDto.java

--- DTO/CartItemDto.java
+++ DTO/CartItemDto.java
@@ -1,12 +1,20 @@
 public class CartItemDto {
-    private ArrayList<ItemDto> items;
+    private List<ItemDto> items = new ArrayList<>();
     private int totalPrice;
 
     public void totalPrice(){
+        int x = 0;
+        for(ItemDto i : items.getItems()){
+            x += i.getSubtotal();
+        }
+        totalPrice = x;
     };
     public void addItem(ItemDto item){
+        items.add(item);
     };
-    public void update(String itemCode, int quantity){
+
+    
+    public void update(String itemCode, int quantity{
     };
 
     public void remove(String itemCode){
