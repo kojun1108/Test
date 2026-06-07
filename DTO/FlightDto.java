f0d8dbd30c3c0d857876deefa0408a5bc5e8cad1
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 12:46:37 +0900

    Update FlightDto.java

--- DTO/FlightDto.java
+++ DTO/FlightDto.java
@@ -1,7 +1,18 @@
 package com.example.travel.dto;
 
-public class FlightCartItemDto
-        extends CartItemDto {
+public class FlightDto
+        extends ItemDto {
+
+private String flightCode;
+private Time departureTime;
+                private Time arrivalTime;
+                private String departureAiportCode;
+                private String basicPrice;
+                private String aiportCode;
+                private Date date;
+                private int price;
+                private int version;
+                private String name;
 
     @Override
     public String getItemType() {
