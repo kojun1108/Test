4c529416e6cd29b93d0613de1284af183a2f0eb3
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 15:48:30 +0900

    Update CartItemDto.java

--- DTO/CartItemDto.java
+++ DTO/CartItemDto.java
@@ -8,17 +8,32 @@ public class CartItemDto {
             x += i.getSubtotal();
         }
         totalPrice = x;
-    };
+    }
+    
     public void addItem(ItemDto item){
         items.add(item);
-    };
-
-    
+    }
+  
     public void update(String itemCode, int quantity{
-    };
+        for(int i =0; i<items.size(); i++){
+            ItemDto item = items.get(i);
+            if(item.getItemCode().equals(itemCode)){
+                item.setQuantity(quantity);
+                break;
+            }
+        }
+    }
 
     public void remove(String itemCode){
-};
+
+        for(int i =0; i<items.size(); i++){
+            ItemDto item = items.get(i);
+            if(item.getItemCode().equals(itemCode)){
+                items.remove(i);
+                break;
+            }
+        }
+    }
 
     
 }
