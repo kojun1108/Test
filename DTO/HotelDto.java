794a6d02600ebdda997bb9da71334ed31e0fd3c4
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 12:49:34 +0900

    Update HotelDto.java

--- DTO/HotelDto.java
+++ DTO/HotelDto.java
@@ -1,8 +1,17 @@
 package com.example.travel.dto;
 
-public class HotelCartItemDto
-        extends CartItemDto {
+public class HotelDto
+        extends ItemDto {
 
+                private String hotelCode;
+                private String cityCode;
+                private String basicPrice;
+                private String grade;
+                private int stock;
+                private Date date;
+                private int price;
+                private int version;
+                private String name;
     @Override
     public String getItemType() {
         return "HOTEL";
