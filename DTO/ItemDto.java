f7ff622c976370a6e926fd42a1e43556c785e72f
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 12:42:54 +0900

    Update and rename CartDto.java to ItemDto.java

---
+++ DTO/ItemDto.java
@@ -0,0 +1,9 @@
+public class ItemDto {
+
+    private String itemCode;
+    private String itemName;
+    private int price;
+    private int quantity;
+    private int subtotal;
+    private int stock;
+}
