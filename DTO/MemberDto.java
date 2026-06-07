a5d88d8602558b66111fceafad4b36288666027a
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 12:54:46 +0900

    Create MemberDto.java

---
+++ DTO/MemberDto.java
@@ -0,0 +1,11 @@
+public MemberDto{
+private String memberCode;
+  private String name;
+  private String password;
+  private String role;
+  private String mail;
+  private String zipCode;
+  private String address;
+  private String prefecture;
+  private String tel;
+}
