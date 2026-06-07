22f7113169a6cf1e8599e67918b5207aa6ca445b
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 17:12:04 +0900

    Create OrderDetailDto.java

---
+++ DTO/OrderDetailDto.java
@@ -0,0 +1,20 @@
+public class OrderDetailDto {
+
+    private Integer orderDetailId;
+
+    private Integer orderMasterId;
+
+    private String itemType;
+
+    private Integer itemId;
+
+    private String itemName;
+
+    private Integer price;
+
+    private Integer quantity;
+
+  private Integer suntotal;
+
+  
+}
