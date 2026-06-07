ac97134bde82acb1bfdb5a1271ff5d4464a66912
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-07 10:57:07 +0900

    Update MemberDao.java

--- Dao/MemberDao.java
+++ Dao/MemberDao.java
@@ -1,1 +1,13 @@
-public
+@Dao
+@ConfigAutowireable
+public interface MemberDao {
+
+    @Select
+    List<FlightEntity> selectAll();
+
+    @Select
+    FlightEntity selectById(Integer flightId);
+
+    @Update(sqlFile = true)
+    int updateSeat(FlightEntity entity);
+}
