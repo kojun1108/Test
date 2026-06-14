ae5cca11eb7cb391d629922dded008c49a7c8c29
kojun1108 <103748453+kojun1108@users.noreply.github.com>
2026-06-14 17:27:33 +0900

    SearchForm.java を更新

--- Hotel/SearchForm.java
+++ Hotel/SearchForm.java
@@ -1,12 +1,15 @@
 package jp.co.example.form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class HotelSearchForm {
 
+    @NotBlank(message = "宿泊日を入力してください")
     private String stayDate;
 
+    @NotBlank(message = "都市を選択してください")
     private String city;
 
     private Integer maxPrice;
